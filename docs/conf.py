0a4dd1a7a93cc2cbce197e8fa030827b19718885
basalganglia <ilkka.kosunen@gmail.com>
2023-07-09 13:16:37 +0200

    black formaater fix

--- docs/conf.py
+++ docs/conf.py
@@ -7,25 +7,25 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 import sphinx_bootstrap_theme
-project = 'Tranformer Tutorial'
-copyright = '2023, Ilkka Kosunen'
-author = 'Ilkka Kosunen'
-release = '0.1'
+
+project = "Tranformer Tutorial"
+copyright = "2023, Ilkka Kosunen"
+author = "Ilkka Kosunen"
+release = "0.1"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-#html_theme = 'alabaster'
+# html_theme = 'alabaster'
 # html_static_path = ['_static']
-html_theme = 'bootstrap'
+html_theme = "bootstrap"
 html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
